25eab1043a4832bc0e377e298d7786a43297ba1c
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-06-30 13:44:28 -0400

    Create main.java
    change

---
+++ main.java
@@ -0,0 +1,29 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Exercise13_15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Prompt for r1
+        System.out.print("Enter rational r1 with numerator and denominator separated by a space: ");
+        BigInteger n1 = input.nextBigInteger();
+        BigInteger d1 = input.nextBigInteger();
+        Rational r1 = new Rational(n1, d1);
+
+        // Prompt for r2
+        System.out.print("Enter rational r2 with numerator and denominator separated by a space: ");
+        BigInteger n2 = input.nextBigInteger();
+        BigInteger d2 = input.nextBigInteger();
+        Rational r2 = new Rational(n2, d2);
+
+        // Display results
+        System.out.println(r1 + " + " + r2 + " = " + r1.add(r2));
+        System.out.println(r1 + " - " + r2 + " = " + r1.subtract(r2));
+        System.out.println(r1 + " * " + r2 + " = " + r1.multiply(r2));
+        System.out.println(r1 + " / " + r2 + " = " + r1.divide(r2));
+        System.out.println(r2 + " is " + r2.doubleValue());
+
+        input.close();
+    }
+}
